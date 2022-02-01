32366be70c4a89817441f4c84b2240b0b3430c9a
Glenn Bacon <glenn@glennbacon.com>
2022-01-31 20:22:24 -0500

    add save state of value

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import PropTypes from 'prop-types';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
